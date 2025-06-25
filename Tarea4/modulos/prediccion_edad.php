4db71fcae5daea567a2e6be73722ff627b661d6b
Jheinel-Brown <109928519+Black-Brown@users.noreply.github.com>
2025-06-25 13:05:38 -0400

    arreglando par de cositas

--- Tarea4/modulos/prediccion_edad.php
+++ Tarea4/modulos/prediccion_edad.php
@@ -21,15 +21,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST'){
             if ($edad < 18) {
                 $categoria = 'joven';
                 $emoji = '👶';
-                $img = 'https://img.freepik.com/foto-gratis/retrato-hombre-foding-sus-manos_171337-15873.jpg';
+                $img = 'https://img.freepik.com/foto-gratis/sonriente-pareja-joven-sentado-bicicleta-contra-fondo-blanco_23-2147893306.jpg?t=st=1750870347~exp=1750873947~hmac=7474fc01f375923567b34182bb6e592cc85204471111fe315d84ddaf7c90742b&w=1380';
             } elseif ($edad < 60) {
                 $categoria = 'adulto';
                 $emoji = '🧑';
-                $img = 'https://img.freepik.com/foto-gratis/retrato-hombre-mediana-edad-seguro_23-2149051743.jpg?t=st=1750868943~exp=1750872543~hmac=03fdf3a9b11328bf9603e30e9c22e1e57297ad14ec470a847628253699b7edab&w=1380';
+                $img = 'https://img.freepik.com/foto-gratis/senior-pareja-navidad-disparo-primer-plano_23-2148333667.jpg';
             } else {
                 $categoria = 'anciano';
                 $emoji = '👨‍🦳';
-                $img = 'https://img.freepik.com/foto-gratis/vista-frontal-anciano-sentado-banco_23-2150493103.jpg?t=st=1750868825~exp=1750872425~hmac=d5909e2e76b7cc5b682c56cceb79cdc61d552ba39564cade8806ed5efb88f6db&w=1380';
+                $img = 'https://img.freepik.com/foto-gratis/pareja-ancianos_23-2148138522.jpg?t=st=1750870305~exp=1750873905~hmac=01972463897c3c1296ffb591a893b53ac63c9e41d95d829f2815ec74a76f2f74&w=1380';
             }
         } else {
             $error = "No se puede predecir la edad para este nombre.";
@@ -53,7 +53,7 @@ plantilla::aplicar();
                     <form action="prediccion_edad.php" method="post">
                         <div class="form-group mb-3">
                             <label for="nombre" class="fw-bold">Nombre:</label>
-                            <input type="text" class="form-control" id="nombre" name="nombre" required <?php echo 'value="' . htmlspecialchars($nombre) . '"'; ?>>
+                            <input type="text" class="form-control" id="nombre" name="nombre" required placeholder="Escribe un nombre..." <?php echo 'value="' . htmlspecialchars($nombre) . '"'; ?>>
                         </div>
                         <div class="d-grid">
                             <button type="submit" class="btn btn-primary">
@@ -66,7 +66,7 @@ plantilla::aplicar();
                         <div class="alert alert-info mt-4 text-center">
                             <strong>Edad:</strong> <?php echo htmlspecialchars($edad); ?> años <br>
                             <strong>Categoría:</strong> <?php echo htmlspecialchars($categoria); ?> <?php echo $emoji; ?><br>
-                            <img src="<?php echo $img; ?>" alt="Imagen representativa" class="img-fluid mt-2" style="max-width: 100%; height: 300px;">
+                            <img src="<?php echo $img; ?>" alt="Imagen representativa" class="img-fluid mt-2" style="max-width: 100%; height: 400px;">
                         </div>
                     <?php elseif ($error): ?>
                         <div class="alert alert-danger mt-3"><?= htmlspecialchars($error) ?></div>
